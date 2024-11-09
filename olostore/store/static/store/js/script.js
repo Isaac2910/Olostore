eb774bf1c35bf666a806feafa3c1798e5bb39e6a
carl07le message <mikolodarselcarl@gmail.com>
2024-11-09 01:36:49 +0100

    deuxieme

--- olostore/store/static/store/js/script.js
+++ olostore/store/static/store/js/script.js
@@ -19,3 +19,16 @@ function initMap() {
         map: map,
     });
 }
+
+
+function increaseQuantity() {
+    let quantity = document.getElementById("quantity");
+    quantity.value = parseInt(quantity.value) + 1;
+}
+
+function decreaseQuantity() {
+    let quantity = document.getElementById("quantity");
+    if (parseInt(quantity.value) > 1) {
+        quantity.value = parseInt(quantity.value) - 1;
+    }
+}
